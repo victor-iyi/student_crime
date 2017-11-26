d6b5b268090e897b4d71127aca1663582e93c82c
victor-iyiola <javafolabi@gmail.com>
2017-11-26 14:43:41 +0100

    Bootstraping application

--- students/views.py
+++ students/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    render(request, template_name='students/index.html')
+    return render(request, template_name='students/index.html')
