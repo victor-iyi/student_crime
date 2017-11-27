3bedfec0fa19f0afcb9b1ef4bdc6cdde2ff281ec
victor-iyiola <javafolabi@gmail.com>
2017-11-27 13:06:52 +0100

    Created the Students and Suspects Model

--- students/models.py
+++ students/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Students(models.Model):
+    name = models.CharField(max_length=255, name='name')
+    matric = models.CharField(max_length=13, name='matric')
+    gender = models.BinaryField(choices=['male', 'female'], name='gender')
+    level = models.IntegerField(name='level')
+
+    def __str__(self):
+        return f'Students<{self.name}, {self.matric}>'
+
+
+class Suspects(models.Model):
+    student = models.ForeignKey(Students, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now=True)
+    description = models.TextField()
