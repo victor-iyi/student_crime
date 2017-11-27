67fd20b5e06655b96a0a19aa78af59a04f52e7a5
victor-iyiola <javafolabi@gmail.com>
2017-11-27 21:43:34 +0100

    Discovered the definition of "subtle"

--- students/models.py
+++ students/models.py
@@ -3,11 +3,14 @@ from django.db import models
 
 class Students(models.Model):
     name = models.CharField(max_length=255, name='name')
-    gender = models.CharField(max_length=6, name='gender')
+    GENDER = (('F', 'female'), ('M', 'male'))
+    gender = models.CharField(max_length=1, choices=GENDER, name='gender')
     image = models.ImageField(width_field=368, height_field=256, name='image')
     matric = models.CharField(max_length=13, name='matric')
     department = models.CharField(max_length=50, name='department')
-    level = models.IntegerField(name='level')
+    LEVEL = (('100', '100 level'), ('200', '200 level'), ('300', '300 level'),
+             ('400', '400 level'), ('500', '500level'))
+    level = models.CharField(max_length=3, choices=LEVEL, name='level')
 
     def __str__(self):
-        return f'Students<{self.name}, {self.matric}>'
+        return f'<{self.name}, {self.matric}>'
