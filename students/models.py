7669eaca029decd91592b3cace64a272440adf9b
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-11-30 09:32:43 +0100

    Update students/models.py

--- students/models.py
+++ students/models.py
@@ -3,14 +3,18 @@ from django.db import models
 
 class Student(models.Model):
     name = models.CharField(max_length=255, name='name')
+    matric = models.CharField(max_length=13, name='matric')
     GENDER = (('F', 'female'), ('M', 'male'))
     gender = models.CharField(max_length=1, choices=GENDER, name='gender')
-    image = models.ImageField(width_field=368, height_field=256, name='image')
-    matric = models.CharField(max_length=13, name='matric')
-    department = models.CharField(max_length=50, name='department')
+    DEPARTMENT = (('ICT', 'Information and Communication Technology'),
+                  ('PES', 'Physical and Earth Sciences'),
+                  ('BIO', 'Biological Sciences'))
+    department = models.CharField(max_length=50, choices=DEPARTMENT, name='department')
     LEVEL = (('100', '100 level'), ('200', '200 level'), ('300', '300 level'),
-             ('400', '400 level'), ('500', '500level'))
-    level = models.CharField(max_length=3, choices=LEVEL, name='level')
+             ('400', '400 level'), ('500', '500 level'))
+    level = models.CharField(max_length=10, choices=LEVEL, name='level')
+
+    image = models.ImageField(width_field=368, height_field=256, name='image')
 
     def __str__(self):
-        return f'<{self.name}, {self.matric}>'
+        return f'{self.name}, {self.matric}'
