247026c1c6878d937efc63c2423eae584df389c0
Victor Iyiola Afolabi <javafolabi@gmail.com>
2017-12-02 21:03:34 +0100

    Update students/views.py

--- students/views.py
+++ students/views.py
@@ -1,3 +1,5 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.views.generic import ListView, DetailView
 
 from students.models import Student
@@ -17,3 +19,18 @@ class Detail(DetailView):
     template_name = 'students/detail.html'
     pk_url_kwarg = 'student_id'
     model = Student
+
+"""
+def e_handle404(request):
+    context = RequestContext(request)
+    response = render_to_response('404.html', context)
+    response.status_code = 404
+    return response
+
+
+def e_handle500(request):
+    context = RequestContext(request)
+    response = render_to_response('500.html', context)
+    response.status_code = 500
+    return response
+"""
